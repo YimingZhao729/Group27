3e3bd172e11f20663f80d71db985b5f77439de32
YimingZhao729 <1261388216@qq.com>
2024-12-16 20:00:57 +0800

    Delete tutorial directory

--- tutorial/tutorial/middlewares.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:be8198dfe30685ef5fd882f3b8d7f5a910933c40753023982b7c7e12b10bc68c
-size 3601
--- tutorial/tutorial/settings.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:80518cdfb6a9458f7bb6c11c3db84986fd833e35489c4d0a820b99a81adad455
-size 3225
